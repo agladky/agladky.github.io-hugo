23ca7c86404f00e4e9002d81500ad1482da2ef61
Anatoly Gladky <anatoliy.gladkiy@gmail.com>
2016-05-23 21:45:23 +0300

    Moved less files in separate folder

--- themes/agladky_theme/less/less-watcher.js
+++ themes/agladky_theme/less/less-watcher.js
@@ -4,7 +4,7 @@ const spawn = require('child_process').spawn;
 process.argv.slice(2).forEach(function(file) {
     fs.watch(file, function(e) {
         if (e === 'change') {
-            spawn('lessc', ['main.less', 'static/css/main.css']);
+            spawn('lessc', ['main.less', '../static/css/main.css']);
         }
     });
 });
