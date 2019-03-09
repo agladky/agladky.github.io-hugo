3c7083a5f52b7258850757e1794aad2ef600aa75
Anatoly Gladky <anatoliy.gladkiy@gmail.com>
2019-03-09 20:50:16 +0300

    Made deploy without submodules

--- deploy.sh
+++ deploy.sh
@@ -6,35 +6,34 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 
-echo -e "\033[0;32mCalculate css...\033[0m"
-cd dev
-make
-cd ..
-
 echo -e "\033[0;32mPull sites...\033[0m"
 rm -rf public/
 mkdir public
 cd public
 git clone git@github.com:agladky/agladky.github.io.git ru
 git clone git@github.com:agladky/agladky.com.git en
+cd ..
+
+echo -e "\033[0;32mCalculate css...\033[0m"
+cd dev
+make
+cd ..
 
 echo -e "\033[0;32mBuild new version...\033[0m"
 hugo
 
 echo -e "\033[0;32mPush ru version...\033[0m"
-cd ru
+cd public/ru
 git add --all
 git commit --message "$msg"
 git push --force origin master
-
-cd ..
+cd ../..
 
 echo -e "\033[0;32mPush en version...\033[0m"
-cd en
+cd public/ru
 git add --all
 git commit --message "$msg"
 git push --force origin master
-
 cd ../..
 
 echo -e "\033[0;32mPush hugo code...\033[0m"
