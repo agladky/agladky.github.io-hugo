0d5ccc3efcb7d31452b6d07b19c44edff0c99e58
Anatoly Gladky <anatoliy.gladkiy@gmail.com>
2019-07-06 13:29:15 +0300

    Added date parser for books

--- dev/goodreads_reviews.py
+++ dev/goodreads_reviews.py
@@ -9,6 +9,7 @@ import re
 import json
 import dataclasses
 
+from datetime import datetime
 from typing import List
 
 # https://makina-corpus.com/blog/metier/2016/the-worlds-simplest-python-template-engine
@@ -16,6 +17,7 @@ TEMPLATE = '''
 +++
 title = "{review.book_title}"
 description = "{review.review_text}"
+date = "{review.read_date_text}"
 +++
 '''
 
@@ -27,21 +29,24 @@ class Review:
     my_rating: str
     review_text: str
     image_url: str
+    read_date_text: str
 
-    
-class EnhancedJSONEncoder(json.JSONEncoder):
-    def default(self, o):
+
+class DataclassesJSONEncoder(json.JSONEncoder):
+    def default(self, o):  # pylint: disable=E0202
         if dataclasses.is_dataclass(o):
             return dataclasses.asdict(o)
         return super().default(o)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Get reviews from https://www.goodreads.com and transform it to Hugo templates")
+    parser = argparse.ArgumentParser(
+        description="Get reviews from https://www.goodreads.com and transform it to Hugo templates"
+        )
     parser.add_argument("-k", "--key", help="Goodreads API key", nargs='?')
     args = parser.parse_args()
 
-    key = args.key
+    key: str = args.key
     if key is None:
         print("Key is not specified. Unnable to load reviews.")
         sys.exit(1)
@@ -54,39 +59,44 @@ if __name__ == "__main__":
         'shelf': 'read',
         'per_page': 200,
         }
-    data = urllib.parse.urlencode(parameters).encode('ascii')
+    data: str = urllib.parse.urlencode(parameters).encode('ascii')
     with urllib.request.urlopen(url, data) as response:
-        response_str = response.read().decode('utf-8')
+        response_str: str = response.read().decode('utf-8')
 
     # example: https://images.gr-assets.com/books/1477845757m/27118836.jpg
     link_pattern = re.compile(r'(\d+)m/(\d+.jpg)')
 
-    root = ET.fromstring(response_str)
+    root: ET.Element = ET.fromstring(response_str)
     reviews: List[Review] = []
+    review: ET.Element
     for review in root.iter('review'):
-        book = review.find('book')
-        book_id = book.find('id').text
+        book: ET.Element = review.find('book')
 
-        book_title = book.find('title').text
-        author_names = ', '.join([a.find('name').text for a in book.iter('author')])
-        my_rating = review.find('rating').text
-        review_text = (
+        book_title: str = book.find('title').text
+        author_names: str = ', '.join([a.find('name').text for a in book.iter('author')])
+        my_rating: str = review.find('rating').text
+        review_text: str = (
             review.find('body').text
             .replace('<br />', '\n')
             .strip())
-        image_url = book.find('image_url').text
+        image_url: str = book.find('image_url').text
         if '/nophoto/' in image_url:
             # print('INFO: No photo for “{}”, {}'.format(book_title, book.find('link').text))
-            large_image_url = image_url
+            large_image_url: str = image_url
         else:
-            large_image_url = link_pattern.sub(r'\1l/\2', image_url)
+            large_image_url: str = link_pattern.sub(r'\1l/\2', image_url)
 
-        reviews.append(Review(book_title, author_names, my_rating, review_text, image_url))
-        
-        
+        read_date_element: ET.Element = review.find('read_at') or review.find('date_added')
+        # Example: Wed Dec 12 11:05:48 -0800 2018
+        read_date: datetime = datetime.strptime(read_date_element.text, '%a %b %d %H:%M:%S %z %Y')
+        # Example: 2015-11-03T13:10:07+0300
+        read_date_text = read_date.strftime('%Y-%m-%dT%H:%M:%S%z')
+
+        reviews.append(Review(book_title, author_names, my_rating, review_text, image_url, read_date_text))
+
     with open('book-reviews.json', 'w') as file:
-        json.dump(reviews, file, cls=EnhancedJSONEncoder, ensure_ascii=False, indent=2)
-    
+        json.dump(reviews, file, cls=DataclassesJSONEncoder, ensure_ascii=False, indent=2)
+
     print(
-      TEMPLATE.format(review=reviews[0]) 
+      TEMPLATE.format(review=reviews[0])
     )
